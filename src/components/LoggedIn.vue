924ef2e1e62594b0e3534504ddf238712f395d9f
Edward <emacc7718@gmail.com>
2018-02-27 19:46:42 -0500

    Dashboard

--- src/components/LoggedIn.vue
+++ src/components/LoggedIn.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="logged-in row" v-if="$store.state.loggedIn">
     <div class="col-md-12">
-      <h2>Welcome, {{ $store.state.currentUser.nickname }}!</h2>
-    </div>
-
-    <div class="col-md-12">
-      <ul>
-        <li><p class="mb-0">Username: {{ $store.state.currentUser["cognito:username"] }}</p></li>
-        <li><p>{{ $store.state.currentUser.email }}</p></li>
-      </ul>
+      <h3>Your Games:</h3>
+      <div v-for="game in games">
+        <img class="gameLogo" :src="game.logo"/>
+      </div>
+      <div class="hr"></div>
+      <h3>Your Groups:</h3>
+      <div class="groupsContainer">
+        <div class="groupCard" v-for="group in groups">
+          <div >
+            <b>{{group.groupName}}</b>
+          </div>
+          <div>Members: {{group.numMembers}}<br/>
+            {{group.region}}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -38,7 +46,19 @@ export default {
       this.$router.replace(this.$route.query.redirect || '/');
     }
   },
-  beforeMount: function () {
+  created: function () {
+    this.$cognitoAuth.isAuthenticated((err, loggedIn) => {
+      if (err) {
+        console.err("Login: Couldn't get the session:", err, err.stack);
+      } else {
+        this.loggedIn = loggedIn;
+        if (!loggedIn) {
+          this.$router.replace(this.$route.query.redirect || '/login');
+        }
+      }
+    });
+  },
+  beforeMount () {
     if (!store.state.loggedIn) {
       this.$router.replace(this.$route.query.redirect || '/login');
     }
@@ -50,5 +70,30 @@ export default {
 <style scoped lang="scss" type="text/scss">
   .logged-in {
     padding-top: 2rem;
+  }
+  .gameLogo {
+    height: 4.5em;
+  }
+  .hr {
+    margin: 15px 0;
+    border-bottom: 1px solid #b5b5b5;
+    width: 95vw;
+  }
+  .groupsContainer {
+    display: flex;
+  }
+  .groupCard {
+    flex-wrap: wrap;
+    align-items: center;
+    margin: 10px 5px;
+    font-size: .8em;
+    background: #e0dfdf;
+    // padding: 10px;
+    border-radius: 8px;
+    width: 120px;
+    min-height: 120px;
+    justify-content: center;
+    text-align: center;
+    overflow: hidden;
   }
 </style>
