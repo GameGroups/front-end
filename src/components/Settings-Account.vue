aac2aad1a45d476961cc255070e6481d679544af
Edward <emacc7718@gmail.com>
2018-03-01 15:30:48 -0500

    Update function is working, user is authenticated before updates occur. Password field added for a user to enter to confirm changes, and is working as expected. Token now refreshes after update so data is consistent in Cognito and the app

--- src/components/Settings-Account.vue
+++ src/components/Settings-Account.vue
@@ -5,20 +5,29 @@
       <p>Edit your information below, and enter your password to confirm changes</p>
     </div>
     <div class="hr"></div>
+
+    <div class="col-sm-6 col-sm-offset-6 form-col">
+    <div class="alert-success" v-if="successMsg.length > 0">{{successMsg}}</div>
     <form @submit.prevent="update">
       <div class="form-group">
         <input type="text" name="nickname" v-model="user.nickname" class="form-control" placeholder="nickname" required>
       </div>
+      <div class="form-group">
+        Enter Password to confirm changes
+        <input type="password" name="password" v-model="user.password" class="form-control" placeholder="password" required>
+      </div>
       <button class="btn btn-primary">Sign Up</button>
     </form>
     <p>token nickname {{this.token.nickname}}</p>
+  </div>
   </div>
 </template>
 
 <script>
 import Vue from 'vue';
 import jwtDecode from 'jwt-decode';
-import { AWSCognito, CognitoUserPool, CognitoUserAttribute, CognitoUser, AuthenticationDetails, CognitoIdentityCredentials, WebIdentityCredentials } from 'amazon-cognito-identity-js';
+import { AWSCognito } from 'aws-sdk'
+import { CognitoUserPool, CognitoUser, AuthenticationDetails } from 'amazon-cognito-identity-js';
 
 export default {
   name: 'Signup',
@@ -32,9 +41,11 @@ export default {
   },
   data: function () {
     return {
-      template: '#account-settings',
+      error: null,
+      successMsg: '',
       user: {
-        nickname: ''
+        nickname: '',
+        password: ''
       }
     }
   },
@@ -46,22 +57,50 @@ export default {
         Value: this.user.nickname
       };
       attributeList.push(attribute);
-      var cognitoUser = this.$cognitoAuth.getCurrentUser();
-      cognitoUser.getSession(function (err, result) {
-        if (result) {
-          cognitoUser.updateAttributes(attributeList, function (err, result) {
+      const cognitoUser = this.$cognitoAuth.getCurrentUser();
 
+      const authenticationData = {
+        Username: cognitoUser.getUsername(),
+        Password: this.user.password
+      };
+      const authenticationDetails = new AuthenticationDetails(authenticationData);
+      const poolData = { UserPoolId: 'us-east-2_aRo6Xa58p',
+        ClientId: '6r5l1vmua09uqgp11i5gs880je'
+      };
+      const userPool = new CognitoUserPool(poolData);
+      const userData = {
+        Username: cognitoUser.getUsername(),
+        Pool: userPool
+      };
+      // const cognitoUser = new CognitoUser(userData);
+      cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function (result) {
+          let refreshToken = cognitoUser.getSignInUserSession().getRefreshToken();
+          /* Use the idToken for Logins Map when Federating User Pools with Cognito Identity or when passing through an Authorization Header to an API Gateway Authorizer */
+
+          cognitoUser.updateAttributes(attributeList, function (err, result) {
             if (err) {
               // this.error = err
               console.error(err)
+            } else {
+              this.successMsg = 'Profile updated successfully!'
+              cognitoUser.refreshSession(refreshToken, function (err, result) {
+                if (err) {
+                  console.log(err);
+                }
+                console.log('idToken + ' + result.idToken.jwtToken);
+                console.log('access token + ' + result.getAccessToken().getJwtToken());
+              })
             }
           });
+        },
+        onFailure: function (err) {
+          if (err.name === 'NotAuthorizedException') {
+            this.error = 'Error: User could not be authenticated. Confirm that your password was entered correctly.'
+          } else {
+            this.error = err;
+          }
         }
-        if (err) {
-          // this.error = err;
-          console.error(err)
-        }
-        console.log(cognitoUser);
       });
     }
   },
@@ -107,5 +146,6 @@ export default {
   }
   .hr {
     border-bottom: 1px solid #b5b5b5;
+    margin-bottom: 25px;
   }
 </style>
