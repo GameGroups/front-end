69bfbb714a57c4c5eec01974c96bbd3fbd6f0120
Peter Holyoke <pholyoke@email.neit.edu>
2018-03-05 16:09:43 -0500

    More progress, profile-header 90% done

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,26 +1,42 @@
 <template>
   <div class="profile row">
-    <div class="d-flex col-md-12">
-      <div class="profile-header">
-        <div class="profile-picture">
-          <img src="http://via.placeholder.com/150x150" />
-        </div>
+    <div class="profile-header col-md-12">
+      <div class="profile-picture">
+        <img src="http://via.placeholder.com/150x150" />
+      </div>
 
+      <div class="primary-info">
         <h2 class="displayname">{{ $store.state.currentUser.nickname }}</h2>
+        <blockquote class="tagline">{{ $store.state.currentUser['custom:tagline'] }}</blockquote>
 
         <ul class="user-stats">
           <li class="skill-level">{{ $store.state.currentUser['custom:skill-level'] }}</li>
           <li class="time-commitment">{{ $store.state.currentUser['custom:time-commitment'] }}</li>
           <li class="time-region">{{ $store.state.currentUser['custom:region'] }}</li>
         </ul>
+
       </div>
 
-      <div class="interaction-bar"></div>
-
-      <div class="profile-bio"></div>
-
-      <div class="groups-sidebar"></div>
+      <div class="secondary-info">
+        <div class="top-games">
+          <ol>
+            <li>Game 1</li>
+            <li>Game 2</li>
+            <li>Game 3</li>
+          </ol>
+        </div>
+      </div>
     </div>
+
+    <div class="interaction-bar col-md-12"></div>
+
+    <div class="profile-bio col-md-9">
+      <p class="bio">
+        {{ $store.state.currentUser['custom:bio'] }}
+      </p>
+    </div>
+
+    <div class="groups-sidebar col-md-3"></div>
   </div>
 </template>
 
@@ -47,7 +63,41 @@ export default {
 <style scoped lang="scss" type="text/scss">
 
   .profile-header {
+    display: flex;
+    padding: 1rem 0 1rem 0;
+    background: orange;
 
+    .primary-info { width: 75%; }
+    .secondary-info { width: 25%; }
+
+    .profile-picture {
+      margin: 0 1rem 0 1rem;
+    }
+
+    .displayname {
+      margin-bottom: 0;
+      padding-bottom: .25rem;
+    }
+
+    .user-stats {
+      justify-content: flex-start;
+      padding: 0;
+
+      li {
+        display: inline;
+      }
+    }
+
+    .tagline {
+      padding-left: .5rem;
+      border-left: 5px solid rgba(6, 6, 6, 1);
+    }
+
+    .top-games {
+      display: flex;
+      justify-content: flex-end;
+      padding-right: 1rem;
+    }
   }
 
 </style>
