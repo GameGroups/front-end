d14a51f964bc6f12e21c6227ff3ff6d9d31e3abf
Edward <emacc7718@gmail.com>
2018-03-08 02:16:31 -0500

    dashboard now has games populating from backend in addition to groups. Added a settings cog which will direct to myaccount/games or myaccount/group. GroupId is passed in the link - need group profile page to be done to finish this page

--- src/components/LoggedIn.vue
+++ src/components/LoggedIn.vue
@@ -1,16 +1,57 @@
 <template>
   <div class="logged-in row" v-if="$store.state.loggedIn">
     <div class="col-md-12">
-      <h2>Your Games:</h2>
-      <div v-for="game in games">
-        <img class="gameLogo" :src="game.logo"/>
+        <router-link to="/myaccount/games" class="gamesTitle">
+          <svg class="edit fa-spin-hover" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1152 896q0-106-75-181t-181-75-181 75-75 181 75 181 181 75 181-75 75-181zm512-109v222q0 12-8 23t-20 13l-185 28q-19 54-39 91 35 50 107 138 10 12 10 25t-9 23q-27 37-99 108t-94 71q-12 0-26-9l-138-108q-44 23-91 38-16 136-29 186-7 28-36 28h-222q-14 0-24.5-8.5t-11.5-21.5l-28-184q-49-16-90-37l-141 107q-10 9-25 9-14 0-25-11-126-114-165-168-7-10-7-23 0-12 8-23 15-21 51-66.5t54-70.5q-27-50-41-99l-183-27q-13-2-21-12.5t-8-23.5v-222q0-12 8-23t19-13l186-28q14-46 39-92-40-57-107-138-10-12-10-24 0-10 9-23 26-36 98.5-107.5t94.5-71.5q13 0 26 10l138 107q44-23 91-38 16-136 29-186 7-28 36-28h222q14 0 24.5 8.5t11.5 21.5l28 184q49 16 90 37l142-107q9-9 24-9 13 0 25 10 129 119 165 170 7 8 7 22 0 12-8 23-15 21-51 66.5t-54 70.5q26 50 41 98l183 28q13 2 21 12.5t8 23.5z"/></svg>
+          <h2 class="games-h2">Your Games</h2>
+        </router-link>
+      <div class="gamesContainer">
+        <div class="game" :key="sub.subscriptionId" v-for="sub in gameSubs">
+          <img v-if="sub.gameName == 'Guild Wars 2'" class="gameLogo" src="../assets/GW2_Logo.png"/>
+          <template v-else>
+            <img class="gameLogo" src="http://via.placeholder.com/150x150"/>
+            <p class="gameLabel">{{sub.gameName}}</p>
+          </template>
+        </div>
       </div>
+
+      <!-- Modal -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Add a Game</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <div class="form-group required gamesList">
+                <select v-validate="'required'" :class="{'form-control': true, 'input-error': errors.has('Games') }" name="Games" required>
+                  <option value="">Select a Game...</option>
+                  <option value="f0b738d4-0165-4425-8bb0-6ca863c77a19">Guild Wars 2</option>
+                </select>
+                <i v-show="errors.has('Games')"></i>
+                <p v-show="errors.has('Games')" class="help-error">{{ errors.first('Games') }}</p>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-primary">Save changes</button>
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div class="hr"></div>
-      <h2>Your Groups:</h2>
+      <router-link to="/myaccount/groups" class="gamesTitle">
+        <svg class="edit fa-spin-hover" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1152 896q0-106-75-181t-181-75-181 75-75 181 75 181 181 75 181-75 75-181zm512-109v222q0 12-8 23t-20 13l-185 28q-19 54-39 91 35 50 107 138 10 12 10 25t-9 23q-27 37-99 108t-94 71q-12 0-26-9l-138-108q-44 23-91 38-16 136-29 186-7 28-36 28h-222q-14 0-24.5-8.5t-11.5-21.5l-28-184q-49-16-90-37l-141 107q-10 9-25 9-14 0-25-11-126-114-165-168-7-10-7-23 0-12 8-23 15-21 51-66.5t54-70.5q-27-50-41-99l-183-27q-13-2-21-12.5t-8-23.5v-222q0-12 8-23t19-13l186-28q14-46 39-92-40-57-107-138-10-12-10-24 0-10 9-23 26-36 98.5-107.5t94.5-71.5q13 0 26 10l138 107q44-23 91-38 16-136 29-186 7-28 36-28h222q14 0 24.5 8.5t11.5 21.5l28 184q49 16 90 37l142-107q9-9 24-9 13 0 25 10 129 119 165 170 7 8 7 22 0 12-8 23-15 21-51 66.5t-54 70.5q26 50 41 98l183 28q13 2 21 12.5t8 23.5z"/></svg>
+        <h2 class="games-h2">Your Groups</h2>
+      </router-link>
       <div class="groupsContainer">
-        <p v-if="groups.length == 0">You haven't joined any groups. Click the groups link at the top to get started!</p>
-        <router-link class="r-link" to="#">
-          <div class="groupCard" v-for="group in groups">
+        <p v-if="!hasGroups">You haven't joined any groups. Click the groups link at the top to get started!</p>
+          <div class="groupCard" :key="group.groupId" v-for="group in groups">
+            <router-link class="r-link" :to="{ path: '/dashboard/group/' + group.groupId, props: {groupID: group.groupId}}" v-on:click="say(asdf)">
             <div class="groupName ">
               <b>{{group.groupName}}</b>
             </div>
@@ -19,8 +60,8 @@
                 <img class="groupImg" src="http://via.placeholder.com/150x150"/>
               </div>
             </div>
+            </router-link>
           </div>
-        </router-link>
       </div>
     </div>
   </div>
@@ -31,6 +72,7 @@ import jwtDecode from 'jwt-decode';
 import Axios from 'axios';
 import store from '../store';
 import cognitoUser from 'amazon-cognito-identity-js'
+import mymodal from './GamesModal'
 
 export default {
   name: 'LoggedIn',
@@ -42,16 +84,19 @@ export default {
       amp: undefined
     }
   },
+  props: ['show'],
   data: function () {
     return {
       loggedIn: false,
-      games: [{
-        gameID: 1,
-        game: 'Guild Wars 2',
-        logo: require('../assets/GW2_Logo.png')
-      }],
-      groups: []
+      hasGroups: false,
+      hasGames: false,
+      gw2Logo: require('../assets/GW2_Logo.png'),
+      groups: [],
+      gameSubs: []
     }
+  },
+  components: {
+    mymodal
   },
   methods: {
     logout: function () {
@@ -80,16 +125,47 @@ export default {
         }
         this.token = jwtDecode(jwtToken);
         this.user = this.$cognitoAuth.getCurrentUser();
-        var config = {
+        let config = {
           headers: { 'Authorization': jwtToken }
         };
-        Axios.get('https://lxcrjbnnlj.execute-api.us-east-2.amazonaws.com/Develop/groups/foruser/' + this.token.sub, config)
+        Axios.all([
+          Axios.get('https://lxcrjbnnlj.execute-api.us-east-2.amazonaws.com/Develop/groups/foruser/' + this.token.sub, config),
+          Axios.get('https://lxcrjbnnlj.execute-api.us-east-2.amazonaws.com/Develop/subscriptions/forentity/' + this.token.sub, config)
+        ])
+          .then(Axios.spread((groups, gameSubs) => {
+            this.groups = groups.data;
+            if (this.groups.length > 0) {
+              this.hasGroups = true;
+            }
+            this.gameSubs = gameSubs.data;
+            if (this.gameSubs.length > 0) {
+              this.hasGames = true;
+            }
+          }))
+          .catch(e => {
+            console.log(e)
+          });
+        /* Axios.get('https://lxcrjbnnlj.execute-api.us-east-2.amazonaws.com/Develop/groups/foruser/' + this.token.sub, config)
           .then(response => {
             this.groups = response.data
+            if (this.groups.length > 0) {
+              this.hasGroups = true;
+            }
           })
           .catch(e => {
             console.log(e)
           })
+        Axios.get('https://lxcrjbnnlj.execute-api.us-east-2.amazonaws.com/Develop/subscriptions/forentity/d3b19d4d-5f74-48e4-a91c-97a317a42c77' + this.token.sub, config)
+          .then(response => {
+            this.gameSubs = response.data
+            console.log('game subs ---------' + this.gameSubs)
+            if (this.gameSubs.length > 0) {
+              this.hasGames = true;
+            }
+          })
+          .catch(e => {
+            console.log(e)
+          }) */
       });
     } else {
       this.$router.replace(this.$route.query.redirect || '/login');
@@ -134,18 +210,86 @@ export default {
     color: #f7c82e;
     overflow: hidden;
     display: -webkit-box;
-    -webkit-line-clamp: 3;
+    -webkit-line-clamp: 2;
     -webkit-box-orient: vertical;
+    text-overflow: ellipsis;
   }
   .groupImg {
     height: 100%;
     width: 100%;
   }
   .r-link {
-    display: flex;
     text-decoration: none;
   }
   .imgContainer {
     height: calc(100% - 3.8em);
+  }
+  .gamesContainer {
+    display: flex;
+    align-items: flex-start;
+  }
+  .gamesTitle {
+    display: flex;
+    align-items: center;
+  }
+  .edit {
+    height: 2.3em;
+    fill: grey;
+  }
+  .edit:hover {
+    cursor: pointer;
+    fill: grey;
+  }
+  .minus {
+    fill: #f35a5a;
+    height: 4em;
+  }
+  .minus:hover {
+    cursor: pointer;
+    fill: red;
+  }
+  .game {
+    margin-right: 15px;
+  }
+  .gamesList {
+    margin: 1rem 0;
+  }
+  .games-h2 {
+    display: inline-block;
+    color: black;
+    margin-left: 15px;
+  }
+  .gameLabel {
+    font-size: .8em;
+    max-width: 5.5em;
+    margin: 0 auto;
+    text-align: center;
+  }
+  .gamesTitle {
+    display: inline-flex;
+    text-decoration: none;
+    margin-bottom: 15px;
+  }
+  @-moz-keyframes spin { 100% { -moz-transform: rotate(360deg); } }
+  @-webkit-keyframes spin { 100% { -webkit-transform: rotate(360deg); } }
+  @keyframes spin { 100% { -webkit-transform: rotate(360deg); transform:rotate(360deg); } }
+  .gamesTitle:hover {
+    .games-h2 {
+      color: #626262;
+    }
+    .edit {
+      -webkit-animation: spin 3s linear infinite ;
+      -moz-animation: spin 3s linear infinite;
+      -o-animation: spin 3s linear infinite;
+      animation: spin 3s linear infinite;
+    }
+
+  }
+  ::-webkit-scrollbar {
+    width: 5px;
+  }
+  /* Track */
+  ::-webkit-scrollbar-track {
+    border-radius: 10px;
   }
 </style>
